8c7a8f553b0f5a878249bd2810a6d94fa4036a17
jppinedal <54005283+jppinedal@users.noreply.github.com>
2022-04-05 23:57:22 -0500

    T4, P4, Py

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -4,5 +4,5 @@ class Asignatura:
         self._nombre = nombre
         self._salon = salon
     def __str__(self):
-        salida = self._nombre +" "+ self._salon 
+        salida = self._nombre+" "+self._salon
         return salida

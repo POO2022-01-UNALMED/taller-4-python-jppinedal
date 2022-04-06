9bb97e69e0b5d410e5e15a69346c776db972942a
jppinedal <54005283+jppinedal@users.noreply.github.com>
2022-04-05 23:23:45 -0500

    T4, P1, Py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,37 +1,56 @@
 
 
 from classroom.asignatura import Asignatura
-from classroom.asignatura import Asignatura
+#from classroom.asignatura import Asignatura
 
 
 class Grupo:
 
-    grado = None
+    grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo ordinado", asignaturas=[], estudiantes=[]):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
+    
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
+
         if(lista is None):
+            lista=[]
             lista.append(alumno)
             self.listadoAlumnos = self.listadoAlumnos + lista
+
+        #else:
+            #self.listadoAlumnos = [alumno]
+
+    def __str__(self):
+        salida = "Grupo de estudiantes: " + self._grupo
+        return salida
+
+    @ classmethod
+    
+
+    def asignarNombre(*args):
+
+      for arg in args:
+        if arg == "Grado 1":
+          Grupo.grado="Grado 1"
+          
         else:
-            self.listadoAlumnos = [alumno]
+          Grupo.grado="Grado 6"
+    
+    #def asignarNombre(cls, nombre="Grado 10"):
+     #   cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
+    #@ classmethod
+    #def asignarNombre(cls, nombre="Grado 6"):
+     #   cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+    #@ classmethod
+    #def asignarNombre(cls, nombre="Grado 4"):
+     #   cls.grado = nombre
